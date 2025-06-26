568b229ae507f09387b88d86ae79c389e91779fa
Eris Verne <ramtinmir.development@gmail.com>
2025-06-26 01:55:43 -0700

    Update AddDeviceView.swift 64

--- HydroPingApp/HydroPing/Views/AuthView.swift
+++ HydroPingApp/HydroPing/Views/AuthView.swift
@@ -69,3 +69,4 @@ struct FloatingBackgroundCircles: View {
                             .strokeBorder(Color.white.opacity(0.4), lineWidth: 6)
                     )
                     .background(
+                        Circle()
