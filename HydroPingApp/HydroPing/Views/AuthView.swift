eabf4597b8602dc7b258262cc12422463d73949f
Eris Verne <ramtinmir.development@gmail.com>
2025-07-01 04:41:08 -0700

    Update AddDeviceView.swift chunk 72

--- HydroPingApp/HydroPing/Views/AuthView.swift
+++ HydroPingApp/HydroPing/Views/AuthView.swift
@@ -78,3 +78,4 @@ struct FloatingBackgroundCircles: View {
                         y: animate1 ? -100 : -200
                     )
                     .animation(.easeInOut(duration: 10).repeatForever(autoreverses: true), value: animate1)
+                
